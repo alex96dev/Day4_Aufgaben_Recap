d07862c94de69d19260a1d0437d7c45fe57185cb
alexanderstracke <alexander.stracke@gmx.net>
2023-07-08 17:21:10 +0200

    File updated till lvl intermediate Task 2

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,16 +1,144 @@
 package de.neuefische;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        String myName = "Alex";
 
 
-
+        System.out.println(name(myName));
+        System.out.print("\n");
+        System.out.println(nameLoop(myName));
+        scanner();
+        System.out.print("\n");
+        stringArray();
+        returnNumbersAsWords();
 
     }
 
     //Anfang Methoden:
+    public static String name(String s) {
+        return s;
+    }
 
 
+    public static String nameLoop(String someString) {
+        String zs = "";
+        for (int i = 0; i < 5; i++) {
+            zs += someString + "\n";
+        }
 
+        return zs;
+    }
+
+
+    public static boolean checkNumber(int number) {
+        if (number > 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+    public static int square(int number) {
+        return number * number;
+    }
+
+
+    public static void scanner() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben Sie Ihren Namen ein und drücken Sie ENTER:");
+        String input = scanner.nextLine();
+
+        System.out.println("\n" + "Herzlich Willkommen! " + input + "!");
+
+    }
+
+    public static void stringArray() {
+        String[] zahlenArray = new String[10];
+
+        zahlenArray[0] = "Eins";
+        zahlenArray[1] = "Zwei";
+        zahlenArray[2] = "Drei";
+        zahlenArray[3] = "Vier";
+        zahlenArray[4] = "Fünf";
+        zahlenArray[5] = "Sechs";
+        zahlenArray[6] = "Sieben";
+        zahlenArray[7] = "Acht";
+        zahlenArray[8] = "Neun";
+        zahlenArray[9] = "Zehn";
+
+        /*System.out.println(zahlenArray[0]);
+        System.out.println(zahlenArray[1]);
+        System.out.println(zahlenArray[2]);
+        System.out.println(zahlenArray[3]);
+        System.out.println(zahlenArray[4]);
+        System.out.println(zahlenArray[5]);
+        System.out.println(zahlenArray[6]);
+        System.out.println(zahlenArray[7]);
+        System.out.println(zahlenArray[8]);
+        System.out.println(zahlenArray[9]);*/
+    }
+
+    public static void returnNumbersAsWords() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie eine einstellige Zahl ein: ");
+        int input = scanner.nextInt();
+
+        String[] zahlenArray = new String[10];
+
+        zahlenArray[0] = "null";
+        zahlenArray[1] = "Eins";
+        zahlenArray[2] = "Zwei";
+        zahlenArray[3] = "Drei";
+        zahlenArray[4] = "Vier";
+        zahlenArray[5] = "Fünf";
+        zahlenArray[6] = "Sechs";
+        zahlenArray[7] = "Sieben";
+        zahlenArray[8] = "Acht";
+        zahlenArray[9] = "Neun";
+
+        switch (input) {
+            case 0:
+                System.out.println(zahlenArray[0]);
+                break;
+            case 1:
+                System.out.println(zahlenArray[1]);
+                break;
+            case 2:
+                System.out.println(zahlenArray[2]);
+                break;
+            case 3:
+                System.out.println(zahlenArray[3]);
+                break;
+            case 4:
+                System.out.println(zahlenArray[4]);
+                break;
+            case 5:
+                System.out.println(zahlenArray[5]);
+                break;
+            case 6:
+                System.out.println(zahlenArray[6]);
+                break;
+            case 7:
+                System.out.println(zahlenArray[7]);
+                break;
+            case 8:
+                System.out.println(zahlenArray[8]);
+                break;
+            case 9:
+                System.out.println(zahlenArray[9]);
+                break;
+            default:
+                System.out.println("nichts");
+                break;
+        }
+
+    }
 }
+
--- src/test/java/de/neuefische/MainTest.java
+++ src/test/java/de/neuefische/MainTest.java
@@ -2,11 +2,86 @@ package de.neuefische;
 
 import org.junit.jupiter.api.Test;
 
+import javax.naming.AuthenticationNotSupportedException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    @Test
+    void return_My_Name() {
+        //GIVEN
+        String someString = "Alex";
+
+        //WHEN
+        String actual = Main.name(someString);
+
+        //THEN
+        assertEquals("Alex", actual);
+    }
+
+    @Test
+    void return_My_Name_5_Times_With_Loop() {
+        //GIVEN
+        String someString = "Alex";
+        int i = 0;
+
+        //WHEN
+        String actual = Main.nameLoop(someString);
+
+        //THEN
+        assertEquals("""
+                Alex
+                Alex
+                Alex
+                Alex
+                Alex
+                """, actual);
+
+    }
+
+    @Test
+    void check_Is_Number_bigger_Than_Zero() {
+        //GIVEN
+        int number = 10;
+
+        //WHEN
+        boolean actual = Main.checkNumber(number);
+
+        //THEN
+        assertEquals(true, actual);
+    }
+
+    @Test
+    void create_Square() {
+        //GIVEN
+        int number = 10;
+
+        //WHEN
+        int actual = Main.square(number);
+
+        //THEN
+        assertEquals(100, actual);
+    }
+
+    /*@Test
+    void create_Array_With_Strings() {
+        //GIVEN
+        String eins = "Eins";
+        String zwei = "zwei";
+        String drei = "drei";
 
 
+        //WHEN
+        int actual = Main.stringArray(s);
+
+        //THEN
+        assertEquals(100, actual);
+    }
+*/
 
 }
+
+
+
+
